eae89ddf9658c556a548991ea30355e656bd8c83
Anastasia Orlova <metkusblackus@gmail.com>
2020-05-05 01:32:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,6 +5,24 @@ def game_hash
     :colors => ["Black", "White"],
     :players => [
       {
+    :player_name => "Alan Anderson",
+    :number => 0,
+    :shoe => 16,
+    :points => 22,
+    :rebounds => 12,
+    :assists => 12,
+    :steals => 3,
+    :blocks => 1
+    :slam_dunks => 1
+    }
+  ]
+  },
+
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => [
+        {
     :player_name =>
     :number =>
     :shoe =>
@@ -16,12 +34,6 @@ def game_hash
     :slam_dunks =>
     }
   ]
-  },
-
-    :away => {
-      :team_name => "Charlotte Hornets",
-      :colors => ["Turquoise", "Purple"],
-      :players => []
 }
   }
 end
