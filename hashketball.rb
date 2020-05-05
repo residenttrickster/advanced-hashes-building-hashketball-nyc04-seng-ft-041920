c7bba0cbc35db66a54b5deb620264131deb927f2
Anastasia Orlova <metkusblackus@gmail.com>
2020-05-05 01:28:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,11 +3,13 @@ def game_hash
     :home => {
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
+    :players => []
   },
 
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
+      :players => []
 }
 
   }
